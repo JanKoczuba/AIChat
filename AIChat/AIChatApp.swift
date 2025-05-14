d528e04575d91ff54c81c73b52364b4a1ef19a13
Jan Koczuba <schizobi@gmail.com>
2025-05-14 19:40:41 +0200

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Jan Koczuba on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
