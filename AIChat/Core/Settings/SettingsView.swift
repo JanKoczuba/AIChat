704d39b5398ef7833ddec0e597f3d7681d303c7f
Jan Koczuba <schizobi@gmail.com>
2025-05-30 14:39:13 +0200

    [Feature] add SettingsView, CreateAccountView

--- AIChat/Core/Settings/SettingsView.swift
+++ AIChat/Core/Settings/SettingsView.swift
@@ -12,16 +12,117 @@ struct SettingsView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState
 
+    @State private var isPremium: Bool = false
+    @State private var isAnonymousUser: Bool = true
+    @State private var showCreateAccountView: Bool = false
+
     var body: some View {
         NavigationStack {
             List {
-                Button {
-                    onSignOutPressed()
-                } label: {
-                    Text("Sign out")
-                }
+                accountSection
+                purchaseSection
+                applicationSection
             }
             .navigationTitle("Settings")
+            .sheet(isPresented: $showCreateAccountView) {
+                CreateAccountView()
+                    .presentationDetents([.medium])
+            }
+        }
+    }
+
+    private var accountSection: some View {
+        Section {
+            if isAnonymousUser {
+                Text("Save & back-up account")
+                    .rowFormatting()
+                    .anyButton(.highlight) {
+                        onAccountCreatePressed()
+                    }
+                    .removeListRowFormatting()
+
+            } else {
+                Text("Sign out")
+                    .rowFormatting()
+                    .anyButton(.highlight) {
+                        onSignOutPressed()
+                    }
+                    .removeListRowFormatting()
+
+            }
+
+            Text("Delete account")
+                .foregroundStyle(.red)
+                .rowFormatting()
+                .anyButton(.highlight) {
+
+                }
+                .removeListRowFormatting()
+        } header: {
+            Text("Account")
+        }
+
+    }
+
+    private var purchaseSection: some View {
+        Section {
+            HStack(spacing: 8) {
+                Text(
+                    "Account status:  \(isPremium ? "Premium" : "FREE")"
+                )
+                Spacer(minLength: 0)
+
+                if isPremium {
+                    Text("MANAGE")
+                        .badgeButton()
+                }
+
+            }
+            .rowFormatting()
+            .anyButton(.highlight) {
+            }
+            .disabled(!isPremium)
+            .removeListRowFormatting()
+
+        } header: {
+            Text("Purchases")
+        }
+    }
+
+    private var applicationSection: some View {
+        Section {
+            HStack(spacing: 8) {
+                Text("Version")
+                Spacer(minLength: 0)
+                Text(Utilities.appVersion ?? "")
+                    .foregroundStyle(.secondary)
+
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            HStack(spacing: 8) {
+                Text("Build Number")
+                Spacer(minLength: 0)
+                Text(Utilities.buildNumber ?? "")
+                    .foregroundStyle(.secondary)
+
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+
+            Text("Contact Us")
+                .foregroundStyle(.blue)
+                .rowFormatting()
+                .anyButton(
+                    .highlight,
+                    action: {
+                        //
+                    }
+                )
+                .removeListRowFormatting()
+
+        } header: {
+            Text("Application")
         }
     }
 
@@ -35,9 +136,24 @@ struct SettingsView: View {
         }
     }
 
+    func onAccountCreatePressed() {
+        showCreateAccountView = true
+    }
+
 }
 
 #Preview {
     SettingsView()
         .environment(AppState(showTabBarView: true))
+}
+
+extension View {
+
+    fileprivate func rowFormatting() -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .background(Color(uiColor: .systemBackground))
+    }
 }
---
+++ AIChat/Utilities/Utilities.swift
@@ -0,0 +1,10 @@
+//
+//  Utilities.swift
+//  AIChat
+//
+//  Created by Jan Koczuba on 30/05/2025.
+//
+
+import SwiftfulUtilities
+
+typealias Utilities = SwiftfulUtilities.Utilities
